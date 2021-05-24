d14cba7e710595e929091d3d5003e723cdc427da
Allan Artuso <allan.artuso@gmail.com>
2021-05-24 03:12:04 +0200

    refactor: cleaning up overlays

--- src/components/Overlays.vue
+++ src/components/Overlays.vue
@@ -7,7 +7,6 @@ export default {
     "overlaysCompList",
     "meta",
     "overlaysCount",
-
     "cursor",
     "colors",
     "interval",
@@ -15,41 +14,31 @@ export default {
     "font",
     "config",
   ],
-  data() {
-    return {
-      layer_events: {},
-    };
-  },
-  watch: {},
-  mounted() {
-    this.layer_events = {
-      "new-grid-layer": (d) => this.$emit("new-grid-layer", d),
-      "delete-grid-layer": (d) => this.$emit("delete-grid-layer", d),
-      "show-grid-layer": (d) => this.$emit("show-grid-layer", d),
-      "redraw-grid": (d) => this.$emit("redraw-grid", d),
-      "layer-meta-props": (d) => this.$emit("layer-meta-props", d),
-      "custom-event": (d) => this.$emit("custom-event", d),
-    };
+  created() {
     this.count = this.overlaysCount;
   },
-  methods: {
-    common_props() {
-      return {
-        cursor: this.$props.cursor,
-        colors: this.$props.colors,
-        layout: this.$props.layoutGrids[this.$props.grid_id],
-        interval: this.$props.interval,
-        sub: this.$props.sub,
-        font: this.$props.font,
-        config: this.$props.config,
-      };
-    },
-
-    createOverlays(h) {
-      return (this.overlaysCompList || []).map((x, i) =>
-        h(x.cls, {
-          on: this.layer_events,
-          attrs: Object.assign(this.common_props(), {
+  render(h) {
+    return h(
+      "div",
+      {},
+      (this.overlaysCompList || []).map((x, i) => {
+        return h(x.cls, {
+          on: {
+            "new-grid-layer": (d) => this.$emit("new-grid-layer", d),
+            "delete-grid-layer": (d) => this.$emit("delete-grid-layer", d),
+            "show-grid-layer": (d) => this.$emit("show-grid-layer", d),
+            "redraw-grid": (d) => this.$emit("redraw-grid", d),
+            "layer-meta-props": (d) => this.$emit("layer-meta-props", d),
+            "custom-event": (d) => this.$emit("custom-event", d),
+          },
+          attrs: {
+            cursor: this.cursor,
+            colors: this.colors,
+            layout: this.layoutGrids[this.grid_id],
+            interval: this.interval,
+            sub: this.sub,
+            font: this.font,
+            config: this.config,
             id: `${x.type}_${this.count[x.type]++}`,
             type: x.type,
             data: x.data,
@@ -60,39 +49,9 @@ export default {
             grid_id: this.grid_id,
             meta: this.meta,
             last: x.last,
-          }),
-        })
-      );
-    },
-    // Replace the current comp with 'renderer'
-  },
-  render(h) {
-    const layout = this.layoutGrids[this.grid_id];
-    const props = {
-      position: {
-        x: 0,
-        y: layout.offset || 0,
-      },
-      attrs: {
-        width: layout.width,
-        height: layout.height,
-        overflow: "hidden",
-      },
-    };
-
-    const createOverlays = this.createOverlays(h);
-
-    return h(
-      "div",
-      {
-        class: `trading-vue-overlays-${this.grid_id}`,
-        style: {
-          left: props.position.x + "px",
-          top: props.position.y + "px",
-          position: "absolute",
-        },
-      },
-      createOverlays
+          },
+        });
+      })
     );
   },
 };
